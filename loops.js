f3aa34e2b34bb871696df8a99fd459e62fb00456
Michael Farber <farbohydrate@gmail.com>
2017-12-05 02:13:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,5 +17,12 @@ function whileLoop(number){
 }
 
 function doWhileLoop(array){
-  
+  do{
+    array.pop();
+  } while(array.length > 0 || !maybeTrue());
+  return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
 }
