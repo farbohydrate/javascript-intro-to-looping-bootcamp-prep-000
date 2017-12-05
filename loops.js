cb9df692e892164c96c0133ec812c87cf029d1ef
Michael Farber <farbohydrate@gmail.com>
2017-12-05 02:09:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,9 @@ function forLoop(array){
 }
 
 function whileLoop(number){
-  let countdown = number;
-  while(countdown > 0){
-    console.log(--countdown)
+  while(number > 0){
+    console.log(--number)
   }
   return 'done';
 }
+
